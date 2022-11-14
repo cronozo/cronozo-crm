d51af287019ac2ebb3a80d94ed850048c9f883b2
Adrian Pranga <adrian@pranga.dev>
2022-11-14 20:09:31 +0100

    add react, tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -57,13 +57,15 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
