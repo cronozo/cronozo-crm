013204fe9642d53680e02d636cd785da7f404656
Adrian Pranga <adrian@pranga.dev>
2022-11-23 17:47:16 +0100

    add basic route for web

--- config/routes.php
+++ config/routes.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
+    $routingConfigurator->import(__DIR__ . '/../src/**/Presentation/Controller/*', 'annotation');
 };
